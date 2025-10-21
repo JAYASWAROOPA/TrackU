1e2e298d96f7a7bb9106e956c713b4d049b2a1c7
JAYASWAROOPA <jayaswaroopa.ec23@bitsathy.ac.in>
2025-10-21 18:34:15 +0530

    made the refreshing

--- components/topAppBar/Styles.tsx
+++ components/topAppBar/Styles.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet} from 'react-native'
+import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
@@ -8,19 +8,27 @@ export const styles = StyleSheet.create({
     padding: 11,
     borderBottomLeftRadius: 20,
     borderBottomRightRadius: 20,
-    gap:12,
-    height:81,
+    gap: 12,
+    height: 81,
+  },
+   avatar: {
+    width: 45,
+    height: 45,
+    borderRadius: 22.5,
+    backgroundColor: '#ec4899',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10,
+  },
+  avatarText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 25,
   },
   name: {
-    color:"white",
-    fontSize:28
+    color: 'white',
+    fontWeight:'600',
+    fontSize: 25,
+    flex: 1, // push children to the right
   },
-  avatar:{
-    width:50,
-    height:50,
-    backgroundColor:"white",
-    borderRadius:100,
-    alignItems: 'center',
-    justifyContent:'center'
-  }
-})
+});
--- components/topAppBar/TopAppBar.tsx
+++ components/topAppBar/TopAppBar.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import { View,Text } from 'react-native';
-import {styles} from './Styles'
-import {TTopBar} from './types';
-export const TopAppBar: React.FC<TTopBar> = ({ children,name }) => {
+import { View, Text } from 'react-native';
+import { styles } from './Styles';
+import { TTopBar } from './types';
+
+export const TopAppBar: React.FC<TTopBar> = ({ children, name }) => {
+  // Capitalize all letters and get the first letter
+  const username = name ? name.toUpperCase() : '';
+  const firstLetter = username ? username[0] : '';
+
   return (
-      <View style={styles.container}>
-        <View style={styles.avatar}>
-          <Text>J</Text>
-        </View>
-      <Text style={styles.name}>{name}</Text>
-      {children}
+    <View style={styles.container}>
+      {/* Avatar */}
+      <View style={styles.avatar}>
+        <Text style={styles.avatarText}>{firstLetter}</Text>
+      </View>
+
+      {/* Username */}
+      <Text style={styles.name}>{username}</Text>
+
+      {/* Right side children */}
+      <View style={styles.rightChildren}>{children}</View>
     </View>
-    
   );
 };
-
